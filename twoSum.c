12074ccc903f174dd7078efbb164a7b7483ec474
rsldnc <119888225+rsldnc@users.noreply.github.com>
2023-03-01 15:09:18 +0300

    Add files via upload

---
+++ twoSum.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+ 
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    int i;
+    int j;
+    int *ret_arr = (int *)malloc(2 *sizeof(int));
+    
+	if(ret_arr == NULL)
+    {
+        *ret_arr = 0;
+        return NULL;
+    }
+    i = 0;
+    while (i < numsSize)
+    {
+        j = i + 1;
+        while (j < numsSize)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                *returnSize = 2;
+                ret_arr[0] = i;
+                ret_arr[1] = j;
+                return ret_arr;
+            }
+            j++;
+        }
+        i++;
+    }
+    *returnSize = 0;
+    free(ret_arr);
+
+    return NULL;
+}
