06b6650c0f54f70d731b550d40f9699bd7a2b281
rsldnc <119888225+rsldnc@users.noreply.github.com>
2023-03-04 15:06:50 +0300

    Update validParentheses.c

--- validParentheses.c
+++ validParentheses.c
@@ -4,8 +4,8 @@
 
 int ft_findtype(char c) 
 {
-    if (c == '(')
-    	return 0;
+	if (c == '(')
+    		return 0;
 	else if (c == '{')
 		return 1;
 	else if (c == '[')
@@ -21,32 +21,32 @@ int ft_findtype(char c)
 
 bool isValid(char* s) 
 {
-    int i = 0;
-    int k = 0;
-    int type;
+	int i = 0;
+	int k = 0;
+	int type;
 	int len = strlen(s);
 	char parentheses[] = { '(', '{', '[', ')', '}', ']' };
-    char *validator = (char*)calloc(len, sizeof(char));
-    
-    if(s == NULL || len % 2 == 1) 
-        return false;
-    if (len == 0)
-        return true;
-    while (s[i]) 
+	char *validator = (char*)calloc(len, sizeof(char));
+	
+	if(s == NULL || len % 2 == 1) 
+        	return false;
+	if (len == 0)
+		return true;
+	while (s[i]) 
 	{
 		type = ft_findtype(s[i]);
-        if (type < 3) 
-        	validator[k++] = s[i];
+        	if (type < 3) 
+        		validator[k++] = s[i];
 		else
 		{
-        	if (k > 0 && validator[k - 1] == parentheses[type - 3])
-            	k--;
-            else
-                return false;
-        }
-        i++;
-    }
-    if (k == 0)
+        		if (k > 0 && validator[k - 1] == parentheses[type - 3])
+            			k--;
+            		else
+                		return false;
+		}
+        	i++;
+	}
+	if (k == 0)
 		return true;
 	else
 		return false; 
