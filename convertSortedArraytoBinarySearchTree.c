3120ca141995a7ae7e4888b3ac10a9bf54805fd6
rsldnc <119888225+rsldnc@users.noreply.github.com>
2023-03-17 09:39:18 +0300

    Add files via upload

---
+++ convertSortedArraytoBinarySearchTree.c
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+#include <stdlib.h>
+
+struct TreeNode *sortedArrayToBST(int *nums, int numsSize)
+{
+	if (numsSize == 0)
+        return NULL;
+	int i = numsSize / 2;
+	struct TreeNode *bst = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+	bst->val = nums[i];
+
+	bst->left = sortedArrayToBST(nums, i);
+	bst->right = sortedArrayToBST(nums + (i + 1), numsSize - (i + 1));
+
+	return bst;
+}
