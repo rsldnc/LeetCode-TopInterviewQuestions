cb340be6f8cebfec58668737640480d2ab832d9c
rsldnc <119888225+rsldnc@users.noreply.github.com>
2023-03-22 10:34:26 +0300

    Add files via upload

---
+++ reverseBits.c
@@ -0,0 +1,13 @@
+uint32_t reverseBits(uint32_t n)
+{
+	uint32_t result = 0;
+	int i = 0;
+	while (i < 32)
+	{
+		result <<= 1;
+		result = (result) | (n & 1);
+		n >>= 1;
+		i++;
+    }
+    return result;
+}
